203802643146ab59e574eacbaa988b6507f7faeb
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2021-05-29 19:40:49 +0300

    2p

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,7 @@ export const getCities = async () => {
 export const getWeatherCity = async (idCity) => {
   try {
     const appid = '2692d95687b9f0aef0d3da58989f4a57'
-    const url = `http://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${appid}&units=metric&lang=ru`
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${appid}&units=metric&lang=ru`
     const response = await axios.get(url)
     return response.data
   } catch (e) {
